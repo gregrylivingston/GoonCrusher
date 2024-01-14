3a37763b06fd13c42c5c23bf1e56a48cc332e587
gregrylivingston <livingsgb@gmail.com>
2024-01-14 14:50:26 -0500

    aspect ratio lottery fix

---
+++ scene/player/menu/settings/graphics.gd
@@ -0,0 +1,19 @@
+extends VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_full_screen_pressed():
+	if $VBoxContainer/HBoxContainer/fullScreen.button_pressed: # Disabled (default)
+		get_tree().root.set_mode(Window.MODE_FULLSCREEN)
+	else: # Fullscreen
+		get_tree().root.set_mode(Window.MODE_WINDOWED)
+
