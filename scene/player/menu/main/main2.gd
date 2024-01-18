d6072e87eae1115cd26954d0f7a4634314f6e4b7
gregrylivingston <livingsgb@gmail.com>
2024-01-18 12:43:21 -0500

    new menu character backgrounds

--- scene/player/menu/main/main2.gd
+++ scene/player/menu/main/main2.gd
@@ -6,20 +6,33 @@ func _ready():
 	Root.mainMenu = self
 	selectCar(Root.cars[Root.cars.keys()[Root.selectedCarNum]])
 	
-	
+var selectCarDelay = 0.3
 func selectCar(car):
 	if is_instance_valid(Root.playerCar):Root.playerCar.free()
 	Root.selectedCar = car
 	Root.playerCar = car.scene.instantiate()
-	$backgroundTexture.texture = Root.playerCar.backgroundPic
-	$backgroundTexture2.texture = Root.playerCar.backgroundPic
-	$charTexture.texture = Root.playerCar.profilePic
 
+	$backgroundTexture2.texture = Root.playerCar.backgroundPic
+	$backgroundTexture2.position = Vector2( -get_viewport().size.x , 0 )
+	$backgroundTexture.position = Vector2( 0, 0)
+	get_tree().create_tween().tween_property($backgroundTexture2, "position" , Vector2(0,0) , selectCarDelay).set_ease(Tween.EASE_IN_OUT)
+	get_tree().create_tween().tween_property($backgroundTexture, "position" , Vector2(get_viewport().size.x,0) , selectCarDelay).set_ease(Tween.EASE_IN_OUT)
+	
+	%charTexture2.texture = Root.playerCar.profilePic
+	%charTexture2.position = Vector2( $Control.size.x , $Control.size.y )
+	get_tree().create_tween().tween_property(%charTexture2, "position" , Vector2( 0 , 0 ) , selectCarDelay).set_ease(Tween.EASE_IN_OUT)
+	get_tree().create_tween().tween_property(%charTexture, "position" , Vector2( $Control.size.x  , $Control.size.y ) , selectCarDelay).set_ease(Tween.EASE_IN_OUT)
 	
 	$carStatsContainer.updateStats()
 	$HBoxContainer/Panel/driverName.text = Root.playerCar.charName
 	$voicePlayer.stream = Root.playerCar.introAudio[randi_range(0 , Root.playerCar.introAudio.size()-1)]
 	$voicePlayer.play()
+	await get_tree().create_timer( selectCarDelay ).timeout
+	$backgroundTexture.texture = Root.playerCar.backgroundPic
+	%charTexture.texture = Root.playerCar.profilePic
+	%charTexture2.position = Vector2( 0 , 0 )
+	
+	
 	
 func _on_next_car_button_pressed():selectNextCar()
 	
