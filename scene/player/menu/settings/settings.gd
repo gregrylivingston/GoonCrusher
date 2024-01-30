6f8a25f9639f8a8131704e71afae7b961ba489f9
gregrylivingston <livingsgb@gmail.com>
2024-01-30 12:12:36 -0500

    new fonts and improvements for settings menu

--- scene/player/menu/settings/settings.gd
+++ scene/player/menu/settings/settings.gd
@@ -4,6 +4,7 @@ extends Button
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	$Panel/TabContainer/VBoxContainer2/HBoxContainer/Gameplay.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,5 +12,23 @@ func _process(delta):
 	pass
 
 
-func _on_pressed():
+
+
+
+func viewMenu(num: int):
+	for i in [$Panel/VBoxContainer/Gameplay, $Panel/VBoxContainer/Sound, $Panel/VBoxContainer/Graphics, $Panel/VBoxContainer/Input]:
+		i.visible = false
+	$Panel/VBoxContainer.get_child(num).visible = true	
+
+
+func _on_gameplay_pressed():viewMenu(0)
+
+func _on_sound_pressed():viewMenu(1)
+
+func _on_graphics_pressed():viewMenu(2)
+
+func _on_input_pressed():viewMenu(3)
+
+
+func _on_continue_pressed():
 	queue_free()
