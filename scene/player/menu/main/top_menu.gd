eda07df7f4748971d112cdff7cfcbdab4bc0ddfa
gregrylivingston <livingsgb@gmail.com>
2024-03-03 16:08:44 -0500

    fix discord link, small unit fixes

--- scene/player/menu/main/top_menu.gd
+++ scene/player/menu/main/top_menu.gd
@@ -20,7 +20,7 @@ func _on_settings_button_pressed():
 	Root.mainMenu.add_child(load("res://scene/player/menu/settings/settings.tscn").instantiate())
 
 func _on_discord_button_pressed():
-	OS.shell_open("https://discord.gg/4m6mENxu")
+	OS.shell_open("https://discord.gg/CRwgEe4Gve")
 
 func _on_steam_button_pressed():
 	OS.shell_open("https://store.steampowered.com/app/1941650/Gregry_Boyds_RidenPrize_Goon_Crusher/")
